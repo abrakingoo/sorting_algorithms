f2fc78c9fae76a214e513d2c62b66ae7b961ac45
freedom469 <bamjnr99@gmail.com>
2023-07-22 11:46:00 +0300

    fixed betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * swap_values - swaps 2 values
+ * @xp: first value
+ * @yp: second value
+ */
+
+void swap_values(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
  * bubble_sort -  sorts an array of integers in ascending order
  * @arr: the array to be sorted
  * @size: size of the array
@@ -10,7 +23,6 @@ void bubble_sort(int *arr, size_t size)
 {
 	size_t i;
 	size_t j;
-	int temp;
 	int swap;
 
 	if (!arr || size < 2)
@@ -26,9 +38,7 @@ void bubble_sort(int *arr, size_t size)
 		{
 			if (arr[j] > arr[j + 1])
 			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				swap_values(&arr[j], &arr[j + 1]);
 				swap = 1;
 				print_array(arr, size);
 			}
--- sort.h
+++ sort.h
@@ -14,12 +14,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-
+void swap_values(int *xp, int *yp);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
